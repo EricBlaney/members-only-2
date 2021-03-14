0204f6f5199c17c21850c976cb0b31499ae191a3
EricBlaney <EricBlaney@gmail.com>
2021-03-14 15:23:51 -0400

    Finalized first functional draft.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "home#index"
+  root "posts#index"
+
+  resources :posts, only: [:new, :create, :index]
+  resources :users, only: [:index]
 end
